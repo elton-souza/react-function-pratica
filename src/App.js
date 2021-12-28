cdbff04c15392a342f8ba14f440f1e11c07c4e83
elton-souza <elton.souza1515@gmail.com>
2021-12-28 17:28:08 -0300

    controlando formulario e criando verificações

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import {Container, Typography} from '@mui/material';
-import "fontsource-roboto"
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import React from "react";
+import "./App.css";
+import { Container, Typography } from "@mui/material";
+import "fontsource-roboto";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 function App() {
+  const aoEnviarForm = (dados) => {
+    console.log(dados);
+  };
+  const validarCpf = (cpf) => {
+    if (cpf.length !== 11) {
+      return { valido: false, texto: "O CPF precisa ter 11 dígitos" };
+    } else {
+      return { valido: true, texto: "" };
+    }
+  };
   return (
     <Container maxWidth="sm">
-      <Typography variant="h3"component="h1" align="center" >Formulario de Cadastro</Typography>
-      <FormularioCadastro/>
+      <Typography variant="h3" component="h1" align="center">
+        Formulario de Cadastro
+      </Typography>
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
     </Container>
   );
 }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,80 @@
-import React from 'react';
-import {Button, TextField, Switch, FormControlLabel} from '@mui/material'
-function FormularioCadastro(){
-    return (
-        <form>
-            <TextField label="Nome" id="nome" variant="outlined" margin="normal" fullWidth/>
-            <TextField label="Sobrenome" id="sobrenome" variant="outlined" margin="normal" fullWidth/>
-            <TextField label="CPF" id="cpf" variant="outlined" margin="normal" fullWidth/>
-            <FormControlLabel label="Promoções" control={<Switch nome="promocoes" defaultChecked color="primary"/>} />
-            <FormControlLabel label="Novidades" control={<Switch nome="novidades" defaultChecked color="primary"/>} />
-            <Button type="submit" variant="contained" color="primary">
-                Cadastrar
-            </Button>
-        </form>
-    )
+import React, { useState } from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
+function FormularioCadastro({aoEnviar, validarCpf}) {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobreNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [novidades, setNovidades] = useState(true);
+  const [promocoes, setPromocoes] = useState(true);
+  const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+        label="Nome"
+        id="nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={sobrenome}
+        onChange={(event) => setSobreNome(event.target.value)}
+        label="Sobrenome"
+        id="sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
+        onBlur={()=>{
+          const updateErrorCpf = validarCpf(cpf);
+          setErros({cpf:updateErrorCpf})
+        }}
+        value={cpf}
+        onChange={(event) => setCpf(event.target.value)}
+        label="CPF"
+        id="cpf"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch
+            nome="promocoes"
+            checked={promocoes}
+            onChange={(event) => setPromocoes(event.target.checked)}
+            color="primary"
+          />
+        }
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={
+          <Switch
+            nome="novidades"
+            checked={novidades}
+            onChange={(event) => setNovidades(event.target.checked)}
+            color="primary"
+          />
+        }
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
